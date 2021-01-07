34af837864f5e90cc9403d9b7dda561b4dce5a44
Derek Peacock <derek.peacock@yahoo.com>
2021-01-07 20:30:47 +0000

    Addded Make Choice and Score Round

--- ConsoleApp.Tests/RPS_GameTests.cs
+++ ConsoleApp.Tests/RPS_GameTests.cs
@@ -20,8 +20,44 @@ namespace ConsoleApp.Tests
             
             Assert.AreEqual(GamePlayers.Player, game.CurrentPlayer.PlayerType);
 
-            Assert.AreEqual("Gita", game.Human.Name);
+            Assert.AreEqual("Tim", game.Human.Name);
             Assert.AreEqual("Computer", game.Computer.Name);
+        }
+
+        [TestMethod]
+        public void TestComputerChoiceIsRandom()
+        {
+            int[] choices = new int[4];
+
+            game.Start();
+
+            for(int i = 0; i < 10000; i++)
+            {
+                game.MakeComputerChoice();
+                int choiceNo = (int)game.Computer.Choice;
+                choices[choiceNo]++;
+            }
+
+            Assert.IsTrue(choices[0] == 0);
+            Assert.IsTrue(choices[1] > 3000);
+            Assert.IsTrue(choices[2] > 3000);
+            Assert.IsTrue(choices[3] > 3000);
+        }
+
+        [TestMethod]
+        public void TestScoreForRockAndPaper()
+        {
+            game.Start();
+
+            game.Human.Choice = GameChoices.Rock;
+            game.Computer.Choice = GameChoices.Paper;
+
+            game.ScoreRound();
+
+            Assert.AreEqual(0, game.Human.Score);
+            Assert.AreEqual(2, game.Computer.Score);
+            Assert.AreEqual(game.Winner.PlayerType, GamePlayers.Computer);
+
         }
     }
 }
--- ConsoleAppProject/App05/Game.cs
+++ ConsoleAppProject/App05/Game.cs
@@ -13,6 +13,8 @@ namespace ConsoleAppProject.App05
 
         public int Round { get; set; }
 
+        public int LastRound { get; set; }
+
         public Player Human { get; }
 
         public Player Computer { get; }
@@ -21,9 +23,11 @@ namespace ConsoleAppProject.App05
 
         public Player Winner { get; set; }
 
+        private Random generator = new Random(55);
+
         public Game(string name)
         {
-            Human = new Player("Gita", GamePlayers.Player);
+            Human = new Player(name, GamePlayers.Player);
             Computer = new Player("Computer", GamePlayers.Computer);
 
             Round = 0;
@@ -32,10 +36,50 @@ namespace ConsoleAppProject.App05
         public void Start()
         {
             Round = 1;
+            LastRound = 5;
+
             CurrentPlayer = Human;
 
             Human.Score = 0;
             Computer.Score = 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void MakeComputerChoice()
+        {
+            int choice = generator.Next(1,4);
+
+            switch(choice)
+            {
+                case 1: Computer.Choice = GameChoices.Paper; break;
+                case 2: Computer.Choice = GameChoices.Rock; break;
+                case 3: Computer.Choice = GameChoices.Scissors; break;
+            }
+        }
+
+        public void ScoreRound()
+        {
+            if(Human.Choice == GameChoices.Rock &&
+               Computer.Choice == GameChoices.Paper)
+            {
+                Computer.Score += 2;
+                Winner = Computer;
+            }
+
+            // 5 other combinations to add
+
+            if (Round < LastRound)
+            {
+                Round++;
+            }
+            else End();
+        }
+
+        public void End()
+        {
+
         }
     }
 }
