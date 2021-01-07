f5fe50d13363cbd8db7ad3cde3a974cb6315037b
Derek Peacock <derek.peacock@yahoo.com>
2021-01-07 17:03:57 +0000

    Added testing of Game Start

---
+++ ConsoleApp.Tests/RPS_GameTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppProject.App05;
+
+namespace ConsoleApp.Tests
+{
+    [TestClass]
+    public class RPS_GameTests
+    {
+
+        private Game game = new Game("Tim");
+
+        [TestMethod]
+        public void TestGameStart()
+        {
+            game.Start();
+
+            Assert.AreEqual(0, game.Human.Score);
+            Assert.AreEqual(0, game.Computer.Score);
+            Assert.AreEqual(1, game.Round);
+            
+            Assert.AreEqual(GamePlayers.Player, game.CurrentPlayer.PlayerType);
+
+            Assert.AreEqual("Gita", game.Human.Name);
+            Assert.AreEqual("Computer", game.Computer.Name);
+        }
+    }
+}
