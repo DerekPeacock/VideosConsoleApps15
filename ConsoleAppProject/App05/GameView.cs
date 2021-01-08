ab763bb9cc322c7d0cd268f7ead19898011bd225
Derek Peacock <derek.peacock@yahoo.com>
2021-01-08 20:19:23 +0000

    Added empty comment

--- ConsoleAppProject/App05/GameView.cs
+++ ConsoleAppProject/App05/GameView.cs
@@ -69,6 +69,9 @@ namespace ConsoleAppProject.App05
             game.Start();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
         private void SetupConsole()
         {
             Console.SetWindowSize(100, 40);
